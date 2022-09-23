616064c52f644683eb3e25e43cd053c1ebd9247a
Richard van Heest <rvanheest@users.noreply.github.com>
2022-09-23 12:32:56 +0200

    basic react electron app

---
+++ public/main.js
@@ -0,0 +1,41 @@
+const { app, BrowserWindow } = require('electron')
+const remoteMain = require('@electron/remote/main')
+
+remoteMain.initialize()
+
+const path = require('path')
+const isDev = require('electron-is-dev')
+
+function createWindow() {
+  const window = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+      enableRemoteModule: true,
+      contextIsolation: false,
+    },
+  });
+
+  remoteMain.enable(window.webContents)
+
+  window.loadURL(
+    isDev
+      ? 'http://localhost:3000'
+      : `file://${path.join(__dirname, '../build/index.html')}`
+  );
+}
+
+app.on('ready', () => createWindow());
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
