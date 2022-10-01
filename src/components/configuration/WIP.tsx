f17cd2c11ad5fa620b46801b13370b55d7cf361e
Richard van Heest <rvanheest@users.noreply.github.com>
2022-10-01 18:27:47 +0200

    More camera operations implemented

--- src/components/configuration/WIP.tsx
+++ src/components/configuration/WIP.tsx
@@ -1,4 +1,5 @@
 import React from "react"
+import { Button } from "react-bootstrap"
 import { AppConfig } from "../../core/config"
 import * as Electron from "electron"
 import { getConfigPath } from "../../core/utils"
@@ -14,7 +15,7 @@ const WIP = ({ config }: WipProps) => {
         <div>
             <h3 className="text-center">WORK IN PROGRESS</h3>
             <p className="text-center fst-italic">Hier kan de configuratie worden ingesteld.</p>
-            <button type="button" className="btn btn-primary" onClick={() => shell.openExternal(getConfigPath())}>Open configuratie</button>
+            <Button onClick={() => shell.openExternal(getConfigPath())}>Open configuratie</Button>
             <div className="border border-secondary border-3 rounded-2 mt-3">
                 <pre className="mb-0" style={{maxHeight: "calc(100vh - 145px)"}}>{JSON.stringify(config, null, 2)}</pre>
             </div>
