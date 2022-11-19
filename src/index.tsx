5e3029a6ae6ffb19ece6efcd6d234ce0fe26760c
Richard van Heest <rvanheest@users.noreply.github.com>
2022-11-19 13:42:35 +0100

    a couple of small fixes

--- src/index.tsx
+++ src/index.tsx
@@ -6,7 +6,7 @@ import './index.css';
 
 import App from './components/App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
     <App />
