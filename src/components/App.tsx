012ca183b43450e07f0e09a8afff6b8f4a2bfad2
Richard van Heest <rvanheest@users.noreply.github.com>
2022-12-20 11:56:51 +0100

    replacing useEffect with state init

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,15 +1,10 @@
-import React, { useEffect, useState } from "react"
+import React, { useState } from "react"
 import ConfigErrorPage from "./ConfigErrorPage"
 import { AppConfig, ConfigError, loadConfig } from "../core/config"
 import TabPane from "./TabPane"
 
 const App = () => {
-  const [config, setConfig] = useState<AppConfig | ConfigError>()
-
-  useEffect(() => {
-    const c = loadConfig()
-    setConfig(c)
-  }, [])
+  const [config] = useState<AppConfig | ConfigError>(loadConfig())
 
   if (!config) {
     return (<div>Loading...</div>)
