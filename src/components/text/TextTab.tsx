91f365c1c2ae1fe6d85b60ba796680a9b2f70dbf
Richard van Heest <rvanheest@users.noreply.github.com>
2023-01-27 00:01:33 +0100

    render components from switch or lookup table as JSX.Element

--- src/components/text/TextTab.tsx
+++ src/components/text/TextTab.tsx
@@ -8,49 +8,37 @@ import BijbellezingTeksten from "./BijbellezingTeksten";
 import CursusGeestelijkeVormingTeksten from "./CursusGeestelijkeVormingTeksten";
 import RouwdienstTeksten from "./RouwdienstTeksten";
 import TrouwdienstTeksten from "./TrouwdienstTeksten";
+import LoadTextStore from "./LoadTextStore";
+
+const tabs = {
+    "Kerkdienst": KerkdienstTeksten,
+    "Bijbellezing": BijbellezingTeksten,
+    "Cursus Geestelijke Vorming": CursusGeestelijkeVormingTeksten,
+    "Huwelijksdienst": TrouwdienstTeksten,
+    "Begrafenisdienst": RouwdienstTeksten,
+}
+const tabsKeys = Object.keys(tabs)
+
+function isNumber(o: unknown): o is Number {
+    return typeof o === "number"
+}
 
 const TextTab = () => {
     const textStorePath = useTextStorePath()
     const lastOpenedTab = useTextStoreLastOpenedTab()
+    const lastOpenedTabString = isNumber(lastOpenedTab) ? tabsKeys[lastOpenedTab] : lastOpenedTab
     const setLastOpenedTextTab = useSetLastOpenedTextTab()
 
-    function saveSelectedTab(index: number): void {
-        if (lastOpenedTab !== index) { // if we update always, we get infinite render recursion
-            setLastOpenedTextTab(index)
-        }
-    }
-
-    const tabs = [
-        {
-            title: "Kerkdienst",
-            element: <KerkdienstTeksten />
-        },
-        {
-            title: "Bijbellezing",
-            element: <BijbellezingTeksten />
-        },
-        {
-            title: "Cursus Geestelijke Vorming",
-            element: <CursusGeestelijkeVormingTeksten />
-        },
-        {
-            title: "Huwelijksdienst",
-            element: <TrouwdienstTeksten />
-        },
-        {
-            title: "Begrafenisdienst",
-            element: <RouwdienstTeksten />
-        }
-    ]
-
     return (
-        <div className="border border-dark border-3 rounded-3 overflow-hidden">
-            <div className="d-flex justify-content-center position-relative">
-                <h3>Teksten</h3>
-                <Button className="position-absolute translate-middle-y top-50 end-0" onClick={async () => await openFile(textStorePath)}>Open JSON</Button>
+        <LoadTextStore>
+            <div className="border border-dark border-3 rounded-3 overflow-hidden">
+                <div className="d-flex justify-content-center position-relative">
+                    <h3>Teksten</h3>
+                    <Button className="position-absolute translate-middle-y top-50 end-0" onClick={async () => await openFile(textStorePath)}>Open JSON</Button>
+                </div>
+                <CardTabPane tabs={tabs} defaultOpen={lastOpenedTabString} onSelectTab={setLastOpenedTextTab} />
             </div>
-            <CardTabPane tabs={tabs} defaultOpenIndex={lastOpenedTab} onSelectTab={saveSelectedTab} />
-        </div>
+        </LoadTextStore>
     )
 }
 
--- src/components/util/CardTabPane.tsx
+++ src/components/util/CardTabPane.tsx
@@ -1,48 +1,40 @@
-import React, { useEffect, useState } from "react"
+import React, { useState } from "react"
 import styling from "./CardTabPane.module.css"
 import { Nav, Tab } from "react-bootstrap";
 
 type TabPaneProps = {
-    tabs: {
-        title: string
-        element: JSX.Element
-    }[]
-    defaultOpenIndex: number
-    onSelectTab?: ((index: number) => void) | undefined
+    tabs: { [title: string]: () => JSX.Element }
+    defaultOpen: string
+    onSelectTab?: ((title: string) => void) | undefined
 }
 
-const CardTabPane = ({ tabs, defaultOpenIndex, onSelectTab }: TabPaneProps) => {
-    const openIndex = Math.max(0, Math.min(tabs.length - 1, defaultOpenIndex))
-    const [selected, setSelected] = useState<string>(tabName(openIndex))
+const CardTabPane = ({ tabs, defaultOpen, onSelectTab }: TabPaneProps) => {
+    const [selected, setSelected] = useState<string>(defaultOpen)
 
-    useEffect(() => {
-        if (onSelectTab) {
-            const tabIndex = tabs.findIndex(tab => selected.startsWith(tab.title))
-            if (tabIndex !== -1) onSelectTab(tabIndex)
+    function onSelect(newSelected: string | null) {
+        if (!!newSelected && newSelected !== selected) {
+            setSelected(newSelected)
+            onSelectTab && onSelectTab(newSelected)
         }
-    }, [selected, onSelectTab, tabs])
-
-    function tabName(index: number) {
-        return `${tabs[index].title}-${index}`
     }
 
     return (
         <div className={`border-0 rounded-bottom rounded-3 bg-light ${styling.card} card`}>
-            <Tab.Container activeKey={selected} onSelect={e => !!e && e !== selected && setSelected(e)}>
+            <Tab.Container activeKey={selected} onSelect={onSelect}>
                 <div className={`px-1 pt-1 pb-0 ${styling.cardHeader} card-header`}>
                     <Nav variant="tabs" className={`${styling.cardNav} border-bottom-0`}>
-                        {tabs.map(({title}, index) => (
-                            <Nav.Item key={tabName(index)}>
-                                <Nav.Link eventKey={tabName(index)} className={styling.cardNavLink}>{title}</Nav.Link>
+                        {Object.keys(tabs).map(title => (
+                            <Nav.Item key={title}>
+                                <Nav.Link eventKey={title} className={styling.cardNavLink}>{title}</Nav.Link>
                             </Nav.Item>
                         ))}
                     </Nav>
                 </div>
 
                 <Tab.Content className="pt-1">
-                    {tabs.map(({element}, index) => (
-                        <Tab.Pane key={tabName(index)} eventKey={tabName(index)}>
-                            {element}
+                    {Object.entries(tabs).map(([title, Element]) => (
+                        <Tab.Pane key={title} eventKey={title}>
+                            <Element />
                         </Tab.Pane>
                     ))}
                 </Tab.Content>
