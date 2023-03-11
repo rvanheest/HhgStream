7be7b486de6fd7de9072495e13a59ca184d12bc9
Richard van Heest <rvanheest@users.noreply.github.com>
2023-03-11 10:03:56 +0100

    Squashed commit of the following:
    commit c2a892135f5ef4bb20db1a609ee556ecf076732a
    Author: Richard van Heest <rvanheest@users.noreply.github.com>
    Date:   Sat Mar 11 10:03:29 2023 +0100
    
        fix formatting for books with special characters
    
    commit 47c637fd00773552541e5d85f0ff873c9cf38929
    Author: Richard van Heest <rvanheest@users.noreply.github.com>
    Date:   Sun Feb 5 20:40:08 2023 +0100
    
        restructure test cases to 'test.each' style

--- tests/core/formatting/dateFormatting.test.ts
+++ tests/core/formatting/dateFormatting.test.ts
@@ -1,8 +1,11 @@
 import {formatDate} from "../../../src/core/formatting/dateFormatting";
 
 describe('formatDate', () => {
-    test('format a date', () => {
-        const date = new Date('1992-07-30T16:04:00');
-        expect(formatDate(date)).toBe('30 juli 1992')
+    const cases = [
+        { date: new Date('1992-07-30T16:04:00'), expectedOutput: '30 juli 1992' },
+    ]
+
+    test.each(cases)('formatDate($date) === $expectedOutput', ({date, expectedOutput}) => {
+        expect(formatDate(date)).toBe(expectedOutput)
     })
 })
--- tests/core/formatting/songFormatting.test.ts
+++ tests/core/formatting/songFormatting.test.ts
@@ -1,125 +1,90 @@
 import { formatSong, formatSongs } from "../../../src/core/formatting/songFormatting"
 
 describe('formatSong', () => {
-    test('a formatted song needs no further formatting', () => {
-        const song = 'Psalm 110 : 1, 2 en 3'
-        expect(formatSong(song)).toBe(song)
-    })
+    const cases = [
+        ['Psalm 110 : 1, 2 en 3', 'Psalm 110 : 1, 2 en 3'],
+        ['Psalm 110:1', 'Psalm 110 : 1'],
+        ['Psalm     117', 'Psalm 117'],
+        ['Gezang 12:4-7', 'Gezang 12 : 4 - 7'],
+        ['Psalm 110 : 1 2 3', 'Psalm 110 : 1, 2 en 3'],
+        ['Psalm 110 : 1, 2, 3', 'Psalm 110 : 1, 2 en 3'],
+        ['Psalm 110:1,2 en 3', 'Psalm 110 : 1, 2 en 3'],
+        ['Psalm 110 : 1, en 2', 'Psalm 110 : 1 en 2'],
+        ['Psalm 18:VZ', 'Psalm 18 : VZ'],
+        ['Psalm 18 : vz', 'Psalm 18 : VZ'],
+        ['Psalm 18 : voorzang, 1, 2', 'Psalm 18 : VZ, 1 en 2'],
+        ['Psalm 18 : VooRZanG', 'Psalm 18 : VZ'],
+        ['Psalm 18: VZ, 1', 'Psalm 18 : VZ en 1'],
+        ['Psalm 18: VZ, 1, 2', 'Psalm 18 : VZ, 1 en 2'],
+        ['U zij de glorie', 'U zij de glorie'],
+    ]
 
-    test('no spaces around colon', () => {
-        expect(formatSong('Psalm 110:1')).toBe('Psalm 110 : 1')
-    })
-
-    test('no verses defined, but with multiple spaces', () => {
-        expect(formatSong('Psalm     117')).toBe('Psalm 117')
-    })
-
-    test('no spaces in verse range', () => {
-        expect(formatSong('Gezang 12:4-7')).toBe('Gezang 12 : 4 - 7')
-    })
-
-    test('no verse formatting', () => {
-        expect(formatSong('Psalm 110 : 1 2 3')).toBe('Psalm 110 : 1, 2 en 3')
-    })
-
-    test('only commas in verse formatting', () => {
-        expect(formatSong('Psalm 110 : 1, 2, 3')).toBe('Psalm 110 : 1, 2 en 3')
-    })
-
-    test('correct styling, no formatting', () => {
-        expect(formatSong('Psalm 110:1,2 en 3')).toBe('Psalm 110 : 1, 2 en 3')
-    })
-
-    test('special case with Voorzang Psalm 18 - spaces', () => {
-        expect(formatSong('Psalm 18:VZ')).toBe('Psalm 18 : VZ')
-    })
-
-    test('special case with Voorzang Psalm 18 - lowercase', () => {
-        expect(formatSong('Psalm 18 : vz')).toBe('Psalm 18 : VZ')
-    })
-
-    test('special case with Voorzang Psalm 18 - full', () => {
-        expect(formatSong('Psalm 18 : voorzang, 1, 2')).toBe('Psalm 18 : VZ, 1 en 2')
-    })
-
-    test('special case with Voorzang Psalm 18 - full lowercase uppercase', () => {
-        expect(formatSong('Psalm 18 : VooRZanG')).toBe('Psalm 18 : VZ')
-    })
-
-    test('special case with Voorzang Psalm 18 - verses', () => {
-        expect(formatSong('Psalm 18: VZ, 1')).toBe('Psalm 18 : VZ en 1')
-    })
-
-    test('special case with Voorzang Psalm 18 - verses multiple', () => {
-        expect(formatSong('Psalm 18: VZ, 1, 2')).toBe('Psalm 18 : VZ, 1 en 2')
-    })
-
-    test('something completely different', () => {
-        const song = 'U zij de glorie'
-        expect(formatSong(song)).toBe(song)
+    test.each(cases)('formatSong(%s) === %s', (song, expectedOutput) => {
+        expect(formatSong(song)).toBe(expectedOutput)
     })
 })
 
 describe('formatSongs', () => {
-    test('align songs with different lengths', () => {
-        const songs = [
-            'Psalm 2 : 1',
-            'Psalm 42 : 1, 3 en 5',
-            'Psalm 119 : 1 en 3',
-            'Psalm 17',
-            'Gezang 2 : 1',
-            'Gezang 12 : 4, 5, 6 en 7',
-            'Psalm 18 : VZ, 1 en 3',
-            'Dit is een ander lied',
-        ]
-        const expected = [
-            'Psalm     2 : 1',
-            'Psalm   42 : 1, 3 en 5',
-            'Psalm 119 : 1 en 3',
-            'Psalm   17',
-            'Gezang   2 : 1',
-            'Gezang 12 : 4, 5, 6 en 7',
-            'Psalm   18 : VZ, 1 en 3',
-            'Dit is een ander lied',
-        ]
-        expect(formatSongs(songs)).toStrictEqual(expected)
-    })
-
-    test('no spacing around colon', () => {
-        const songs = [
-            'Psalm 1:1',
-            'Psalm 2 :1',
-            'Psalm 3: 1',
-        ]
-        const expected = [
-            'Psalm 1 : 1',
-            'Psalm 2 : 1',
-            'Psalm 3 : 1',
-        ]
-        expect(formatSongs(songs)).toStrictEqual(expected)
-    })
-
-    test('formatting of verses', () => {
-        const songs = [
-            'Psalm 2 : 1',
-            'Psalm 42 : 1 3 5',
-            'Psalm 119 : 1,3',
-            'Psalm 17',
-            'Gezang 2 : 1',
-            'Gezang 12 : 4, 5, 6, 7',
-            'Psalm 18 : VZ, 1, 3',
-            'Dit is een ander lied',
+    const cases = [
+        [
+            [
+                'Psalm 2 : 1',
+                'Psalm 42 : 1, 3 en 5',
+                'Psalm 119 : 1 en 3',
+                'Psalm 17',
+                'Gezang 2 : 1',
+                'Gezang 12 : 4, 5, 6 en 7',
+                'Psalm 18 : VZ, 1 en 3',
+                'Dit is een ander lied',
+            ],
+            [
+                'Psalm     2 : 1',
+                'Psalm   42 : 1, 3 en 5',
+                'Psalm 119 : 1 en 3',
+                'Psalm   17',
+                'Gezang   2 : 1',
+                'Gezang 12 : 4, 5, 6 en 7',
+                'Psalm   18 : VZ, 1 en 3',
+                'Dit is een ander lied',
             ]
-        const expected = [
-            'Psalm     2 : 1',
-            'Psalm   42 : 1, 3 en 5',
-            'Psalm 119 : 1 en 3',
-            'Psalm   17',
-            'Gezang   2 : 1',
-            'Gezang 12 : 4, 5, 6 en 7',
-            'Psalm   18 : VZ, 1 en 3',
-            'Dit is een ander lied',
+        ],
+        [
+            [
+                'Psalm 1:1',
+                'Psalm 2 :1',
+                'Psalm 3: 1',
+            ],
+            [
+                'Psalm 1 : 1',
+                'Psalm 2 : 1',
+                'Psalm 3 : 1',
             ]
-        expect(formatSongs(songs)).toStrictEqual(expected)
+        ],
+        [
+            [
+                'Psalm 2 : 1',
+                'Psalm 42 : 1 3 5',
+                'Psalm 119 : 1,3',
+                'Psalm 17',
+                'Gezang 2 : 1',
+                'Gezang 12 : 4, 5, 6, 7',
+                'Psalm 18 : VZ, 1, 3',
+                'Dit is een ander lied',
+            ],
+            [
+                'Psalm     2 : 1',
+                'Psalm   42 : 1, 3 en 5',
+                'Psalm 119 : 1 en 3',
+                'Psalm   17',
+                'Gezang   2 : 1',
+                'Gezang 12 : 4, 5, 6 en 7',
+                'Psalm   18 : VZ, 1 en 3',
+                'Dit is een ander lied',
+            ]
+        ],
+    ]
+
+    test.each(cases)('formatSongs(%s) === %s', (songs, expectedOutput) => {
+        expect(formatSongs(songs)).toStrictEqual(expectedOutput)
     })
 })
