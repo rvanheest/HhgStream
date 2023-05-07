4cf9382503509c04cc1db5a571fa42a465ab3e28
Richard van Heest <rvanheest@users.noreply.github.com>
2023-05-07 14:04:37 +0200

    Squashed commit of the following:
    commit 395f625da6c47d0c12e324c5469a23fc9993ec63
    Author: Richard van Heest <rvanheest@users.noreply.github.com>
    Date:   Sun May 7 14:03:49 2023 +0200
    
        fix merge conflict

--- src/components/util/CardTabPane.tsx
+++ src/components/util/CardTabPane.tsx
@@ -31,13 +31,13 @@ const CardTabPane = ({ tabs, defaultOpen, onSelectTab }: TabPaneProps) => {
                     </Nav>
                 </div>
 
-                <Tab.Content>
+                <Tab.Content className="pt-1">
                     {Object.entries(tabs).map(([title, Element]) => (
                         <Tab.Pane key={title} eventKey={title}>
                             <Element />
                         </Tab.Pane>
                     ))}
-                </Tab.Content>>
+                </Tab.Content>
             </Tab.Container>
         </div>
     )
