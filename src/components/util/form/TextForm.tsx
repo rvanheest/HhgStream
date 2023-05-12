9e1d9bb65852b365412b35bfc1dc19aba1d3eced
Richard van Heest <rvanheest@users.noreply.github.com>
2023-05-12 23:33:06 +0200

    poging 3 om scrollbalk te fixen

--- src/components/util/form/TextForm.tsx
+++ src/components/util/form/TextForm.tsx
@@ -1,6 +1,6 @@
 import React, { ReactNode } from "react"
 import styling from "./TextForm.module.css"
-import { Button, Col, Container, Form, Row } from "react-bootstrap"
+import { Button, Form } from "react-bootstrap"
 
 type TextFormProps = {
     children: ReactNode
@@ -11,22 +11,17 @@ type TextFormProps = {
 const TextForm = ({ children, onClear, onSubmit }: TextFormProps) => {
     return (
         <Form>
-            <Container fluid>
-                <Row className="pt-1 pb-1">
-                    <div className={`${styling.scrollable}`}>
-                        { children }
-                    </div>
-                </Row>
-
-                <Row className="pt-1 pb-1" style={{borderTop: "var(--bs-card-border-width) solid var(--bs-card-border-color)"}}>
-                    <Col sm={{offset: 3, span: 1}}>
-                        <Button variant="primary" onClick={onSubmit}>Opslaan</Button>
-                    </Col>
-                    <Col sm={{span: 2}}>
-                        <Button variant="secondary" onClick={onClear}>Leeg maken</Button>
-                    </Col>
-                </Row>
-            </Container>
+            <div className={`pt-2 ${styling.scrollable}`}>
+                { children }
+            </div>
+            <div className="py-1 d-flex flex-row flex-wrap" style={{ borderTop: "var(--bs-card-border-width) solid var(--bs-card-border-color)" }}>
+                <div style={{ marginLeft: "25%" }}>
+                    <Button variant="primary" onClick={onSubmit}>Opslaan</Button>
+                </div>
+                <div style={{ marginLeft: "35px" }}>
+                    <Button variant="secondary" onClick={onClear}>Leeg maken</Button>
+                </div>
+            </div>
         </Form>
     )
 }
